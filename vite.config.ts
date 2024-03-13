55fc0c7b4abf77af61e6652327f5092a4acd5d75
Trysten M <nick.tristen22@gmail.com>
2024-03-13 23:00:05 +0200

    update (config/vite): update config to git-pages

--- vite.config.ts
+++ vite.config.ts
@@ -1,23 +1,28 @@
-import { defineConfig } from 'vite'
+import {defineConfig} from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 import autoprefixer from "autoprefixer";
 import * as path from "node:path";
-export default defineConfig({
-  plugins: [vue()],
-  css: {
-    postcss: {
-      plugins: [autoprefixer],
-    },
-    preprocessorOptions: {
-      scss: {
-        additionalData: `@import "src/assets/css/variables.scss";`,
-      }
+
+export default defineConfig(({command}) => {
+    const base = command === 'build' ? '/to-webp-converter-client/' : '/'
+    return {
+        plugins: [vue()],
+        css: {
+            postcss: {
+                plugins: [autoprefixer],
+            },
+            preprocessorOptions: {
+                scss: {
+                    additionalData: `@import "src/assets/css/variables.scss";`,
+                }
+            }
+        },
+        resolve: {
+            alias: {
+                '@': path.resolve(__dirname, './src')
+            }
+        },
+        base
     }
-  },
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src')
-    }
-  },
 })
