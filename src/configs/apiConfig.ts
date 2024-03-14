ca7eef2b796292c0e814966c73e378dfd706764a
Trysten M <nick.tristen22@gmail.com>
2024-03-14 12:54:27 +0200

    hotfix(app/config): fix api url

--- src/configs/apiConfig.ts
+++ src/configs/apiConfig.ts
@@ -1,7 +1,6 @@
+
+const apiUrl = import.meta?.env?.VITE_API_URL ? import.meta?.env?.VITE_API_URL : 'https://to-webp-converter.onrender.com/api'
 export default {
-    API_URL: import.meta.env.PROD ?
-        (import.meta.env?.VITE_API_URL || 'https://to-webp-converter.onrender.com/api') :
-        'http://localhost:5000/api',
+    API_URL: import.meta.env.PROD ? apiUrl : 'http://localhost:5000/api',
     IS_ONLINE_REQUEST_TIMEOUT: 1000 * 60 * 14
 }
-console.log('import.meta.env', import.meta.env)
