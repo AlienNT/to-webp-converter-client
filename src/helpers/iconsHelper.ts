a0e49eebf414821d3a9b9fdc726d663fb91efda3
Trysten M <nick.tristen22@gmail.com>
2024-03-14 12:33:36 +0200

    hotfix(app/helpers): fix icons path

--- src/helpers/iconsHelper.ts
+++ src/helpers/iconsHelper.ts
@@ -1,8 +1,8 @@
-//@ts-ignore
-const iconsPath: string = 'icons' + (import.meta.env.BASE_URL ? import.meta.env.BASE_URL : '/')
+console.log('import.meta.env.BASE_URL', import.meta.env.BASE_URL)
+export const iconPath = (import.meta.env.BASE_URL || '') + 'icons/'
 
 export const iconNames =  {
-    DOWNLOAD: iconsPath + 'download-svgrepo-com.svg',
-    REMOVE: iconsPath + 'remove-square-svgrepo-com.svg',
-    CONVERT: iconsPath + 'convert-card-svgrepo-com.svg',
+    DOWNLOAD: iconPath + 'download-svgrepo-com.svg',
+    REMOVE: iconPath + 'remove-square-svgrepo-com.svg',
+    CONVERT: iconPath + 'convert-card-svgrepo-com.svg',
 }
