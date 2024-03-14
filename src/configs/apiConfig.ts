f33f35fed165bc2f7495b734062fa1de6700a6bb
Trysten M <nick.tristen22@gmail.com>
2024-03-14 11:38:20 +0200

    update(config): add request timeout

--- src/configs/apiConfig.ts
+++ src/configs/apiConfig.ts
@@ -1,3 +1,4 @@
 export default {
-    API_URL: import.meta.env.PROD ? import.meta.env?.VITE_API_URL || 'https://to-webp-converter.onrender.com/api' : 'http://localhost:5000/api'
+    API_URL: import.meta.env.PROD ? import.meta.env?.VITE_API_URL || 'https://to-webp-converter.onrender.com/api' : 'http://localhost:5000/api',
+    IS_ONLINE_REQUEST_TIMEOUT: 1000 * 60 * 14
 }
