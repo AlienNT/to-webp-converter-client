c557a0262101d1849a74852ac3a7cd55f8d654f4
Trysten M <nick.tristen22@gmail.com>
2024-03-16 23:31:41 +0200

    update(components/converter): update style converter form header

--- src/components/Converter/ConverterFormHeader.vue
+++ src/components/Converter/ConverterFormHeader.vue
@@ -1,12 +1,24 @@
 <script setup lang="ts">
 
-import {iconNames} from "@/helpers/iconsHelper.ts";
 import IconButton from "@/components/UI/IconButton.vue";
+
 import {IConverterFormHeaderProps} from "@/interfaces/propsInterfaces.ts";
 
-defineProps<IConverterFormHeaderProps>()
+import {iconNames} from "@/helpers/iconsHelper.ts";
+import {computed} from "vue";
+import {colors} from "@/helpers/colorsHelper.ts";
+
+const props = defineProps<IConverterFormHeaderProps>()
 
 const emit = defineEmits(['onRemove'])
+
+const shortedTitle = computed(() => {
+  return props.imageName ? cutString(props.imageName, 60) : props.imageName
+})
+
+function cutString(str: string, maxLength: number): string {
+  return str && str.length > maxLength ? str.slice(0, maxLength - 3) + '...' : str
+}
 </script>
 
 <template>
@@ -15,10 +27,14 @@ const emit = defineEmits(['onRemove'])
         v-if="iconNames"
         class="image-name"
     >
-      {{ imageName }}
+      <div class="title">
+        {{ shortedTitle }}
+      </div>
     </div>
     <IconButton
         :icon="iconNames.REMOVE"
+        :color="colors.REMOVE"
+        use-transform
         title="remove"
         @on-click="emit('onRemove')"
     />
@@ -31,5 +47,26 @@ const emit = defineEmits(['onRemove'])
   display: flex;
   margin-bottom: 15px;
   justify-content: space-between;
+  @media #{$mediumScreen} {
+    margin-bottom: 10px;
+  }
+}
+
+.image-name {
+  flex: auto;
+  text-align: left;
+  font-size: 16px;
+  transition: .2s ease;
+
+  .title {
+    width: 95%;
+    overflow: hidden;
+    text-overflow: ellipsis;
+    word-break: break-all;
+  }
+
+  @media #{$mediumScreen} {
+    font-size: 14px;
+  }
 }
 </style>
