dac0483d9ec8cb1547856da22e8ca690c7012f3b
Trysten M <nick.tristen22@gmail.com>
2024-03-16 23:32:31 +0200

    update(components/converter): update style converter viewport

--- src/components/Converter/ConverterViewport.vue
+++ src/components/Converter/ConverterViewport.vue
@@ -1,9 +1,10 @@
 <script setup lang="ts">
 
 import ConverterForm from "@/components/Converter/ConverterForm.vue";
-import {TUuid} from "@/interfaces/convertingFormInterfaces.ts";
 
 import {useImageActions} from "@/composables/useImageActions.ts";
+
+import {TUuid} from "@/interfaces/convertingFormInterfaces.ts";
 import {IConverterViewportProps} from "@/interfaces/propsInterfaces.ts";
 
 defineProps<IConverterViewportProps>()
@@ -16,32 +17,34 @@ function removeHandler(uuid: TUuid) {
 </script>
 
 <template>
-  <div class="converter-title">
-    To Webp Converter
+  <div class="container">
+    <div class="converter-title">
+      To Webp Converter
+    </div>
+    <ul class="images-list">
+      <li
+          v-for="(imageState, index) in imagesList"
+          class="images-list__item"
+          :key="index"
+      >
+        <ConverterForm
+            :image-name="imageState.image?.name"
+            :image-size="imageState.image?.size"
+            :image-src="imageState.image?.src"
+            :file="imageState.image?.file"
+            :uuid="imageState.image?.uuid"
+            :converting-event="convertingEvent"
+            @on-remove="removeHandler"
+        />
+      </li>
+    </ul>
   </div>
-  <ul class="images-list">
-    <li
-        v-for="(imageState, index) in imagesList"
-        class="images-list__item"
-        :key="index"
-    >
-      <ConverterForm
-          :image-name="imageState.image?.name"
-          :image-size="imageState.image?.size"
-          :image-src="imageState.image?.src"
-          :file="imageState.image?.file"
-          :uuid="imageState.image?.uuid"
-          :converting-event="convertingEvent"
-          @on-remove="removeHandler"
-      />
-    </li>
-  </ul>
 </template>
 
 <style scoped lang="scss">
 .converter-title {
   font-size: 24px;
-  margin-bottom: 15px;
+  margin-bottom: 25px;
 }
 
 .images-list {
